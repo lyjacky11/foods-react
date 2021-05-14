789eeb583ffe98e1dacecb485f1f203b00303d82
lyjacky11 <lyjacky@live.com>
2021-05-14 00:25:45 -0400

    Minor styling

--- src/Place.js
+++ src/Place.js
@@ -3,12 +3,14 @@ const MAPS_API = process.env.REACT_APP_MAPS_API;
 const Place = ({ id, name, category, address, city, province, postal }) => {
   return (
     <div className="place-container">
-      <h2>{name}</h2>
-      <h3>Category: {category}</h3>
-      <h4>{address}</h4>
-      <h4>
-        {city}, {province} {postal}
-      </h4>
+      <div className="place-details">
+        <h2>{name}</h2>
+        <h3>Category: {category}</h3>
+        <h4>{address}</h4>
+        <h4>
+          {city}, {province} {postal}
+        </h4>
+      </div>
       {/* <div className="embed-map">
         <iframe
           loading="lazy"
